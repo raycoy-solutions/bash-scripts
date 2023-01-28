6288997f5545a49ad327b6237c6905716ab10559
Tee <tee@example.com>
2023-01-28 13:28:14 -0700

    Update scripts

--- 11.2.loop.sh
+++ 11.2.loop.sh
@@ -2,8 +2,11 @@
 
 number=1
 
-while [[ $number -lt 10 ]]
+while [[ $number -le 10 ]]
 do
     echo $number
     number=$(( $number + 1 ))
+    sleep 1
 done
+
+echo "Loop has stopped"
--- 8.4.whichuser.sh
+++ 8.4.whichuser.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 userId=`id -u`
 
